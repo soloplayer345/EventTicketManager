bfa48578ad04c5454952680899ff227c76e5f02d
soloplayer345 <hongtlse172336@fpt.edu.vn>
2025-11-12 06:22:10 +0700

    Refactor entities and add account-related models
    Removed InventoryEntity and UserEntity classes and introduced new entities: Account, Organizer, Sponsor, Order, and SponsorEvent. This refactor aligns the data model with new requirements, consolidating user and account management and supporting organizer and sponsor relationships.

---
+++ Inventory BE/Inventory.DLL/Entities/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.DLL.Entities
+{
+    public class Account : BaseEntity
+    {
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Role { get; set; } = null!;
+
+        // Navigation properties
+        public Organizer? Organizer { get; set; }
+        public Sponsor? Sponsor { get; set; }
+        public ICollection<Order>? Orders { get; set; }
+    }
+}
